8e612f86ce196a5bc40696d101965ee549b9e8db
Jeril <jerilbills@gmail.com>
2024-06-14 17:15:42 -0600

    Create ItemDatabaseEntity class

---
+++ ProjectStarter-DONOTOVERWRITE/dotnet/Capstone/DatabaseEntities/ItemDatabaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Capstone.DatabaseEntities
+{
+    public class ItemDatabaseEntity
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int quantity { get; set; }
+    }
+}
